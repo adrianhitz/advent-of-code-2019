5bba9584ffee6f3481707c35042f63e78fccdd21
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-04 06:36:18 +0100

    Day 04 part 2 solution

--- src/main/scala/adrianhitz/adventofcode/Day04.scala
+++ src/main/scala/adrianhitz/adventofcode/Day04.scala
@@ -21,14 +21,32 @@ object Day04 extends AdventIO {
         if(digits(i) > digits(i + 1)) increasing = false
         if(digits(i) == digits(i + 1)) neighbours = true
       }
-
-      for(i <- Range(0, 6)) print(digits(i))
-      println()
-
       if(increasing && neighbours) i += 1
     }
     i
   }
 
-  def part2(implicit s: String): Int = ???
+  def part2(implicit s: String): Int = {
+    val in = s.split('-').map(_.toInt)
+    var i = 0
+    for(n <- Range(in(0), in(1))) {
+      val digits = new Array[Int](6)
+      for(i <- Range(0, 6)) {
+        digits(6 - 1 - i) = (n % Math.pow(10, i + 1).toInt) / Math.pow(10, i).toInt
+      }
+      var increasing = true
+      var neighbours = false
+
+      for(i <- Range(0, 6 - 1)) {
+        if(digits(i) > digits(i + 1)) increasing = false
+        if(digits(i) == digits(i + 1)) {
+          if(digits.count(x => x == digits(i)) <= 2)
+            neighbours = true
+        }
+      }
+
+      if(increasing && neighbours) i += 1
+    }
+    i
+  }
 }
