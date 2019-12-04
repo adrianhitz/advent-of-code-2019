e8190c4051ce375e339af51ead0a77e2f53a4dba
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-04 07:02:47 +0100

    Improve day 04 solution

--- src/main/scala/adrianhitz/adventofcode/Day04.scala
+++ src/main/scala/adrianhitz/adventofcode/Day04.scala
@@ -8,39 +8,41 @@ object Day04 extends AdventIO {
 
   def part1(implicit s: String): Int = {
     val in = s.split('-').map(_.toInt)
-    var i = 0
-    for(n <- Range(in(0), in(1))) {
-      val digits = new Array[Int](6)
-      for(i <- Range(0, 6)) {
-        digits(6 - 1 - i) = (n % Math.pow(10, i + 1).toInt) / Math.pow(10, i).toInt
-      }
-      var increasing = true
-      var neighbours = false
-      for(i <- Range(0, 6 - 1)) {
-        if(digits(i) > digits(i + 1)) increasing = false
-        if(digits(i) == digits(i + 1)) neighbours = true
-      }
-      if(increasing && neighbours) i += 1
-    }
-    i
+    Range(in(0), in(1)).count(n => valid1(splitDigits(n)))
   }
 
   def part2(implicit s: String): Int = {
     val in = s.split('-').map(_.toInt)
-    var i = 0
-    for(n <- Range(in(0), in(1))) {
-      val digits = new Array[Int](6)
-      for(i <- Range(0, 6)) {
-        digits(6 - 1 - i) = (n % Math.pow(10, i + 1).toInt) / Math.pow(10, i).toInt
-      }
-      var increasing = true
-      var neighbours = false
-      for(i <- Range(0, 6 - 1)) {
-        if(digits(i) > digits(i + 1)) increasing = false
-        if(digits.count(x => x == digits(i)) == 2) neighbours = true
-      }
-      if(increasing && neighbours) i += 1
+    Range(in(0), in(1)).count(n => valid2(splitDigits(n)))
+  }
+
+  def splitDigits(n: Int): Array[Int] = {
+    val s = n.toString
+    val d = s.length
+    val digits = new Array[Int](d)
+    for(i <- Range(0, d)) {
+      digits(d - 1 - i) = (n % Math.pow(10, i + 1).toInt) / Math.pow(10, i).toInt
     }
-    i
+    digits
+  }
+
+  def valid1(digits: Array[Int]): Boolean = {
+    var increasing = true
+    var neighbours = false
+    for(i <- Range(0, digits.length - 1)) {
+      if(digits(i) > digits(i + 1)) increasing = false
+      if(digits(i) == digits(i + 1)) neighbours = true
+    }
+    increasing && neighbours
+  }
+
+  def valid2(digits: Array[Int]): Boolean = {
+    var increasing = true
+    var neighbours = false
+    for(i <- Range(0, digits.length - 1)) {
+      if(digits(i) > digits(i + 1)) increasing = false
+      if(digits.count(x => x == digits(i)) == 2) neighbours = true
+    }
+    increasing && neighbours
   }
 }
