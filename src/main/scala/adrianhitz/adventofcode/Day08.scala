7845138df96539d238d04fc199a0745549bec2d6
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-08 08:29:19 +0100

    Add day 08 files

---
+++ src/main/scala/adrianhitz/adventofcode/Day08.scala
@@ -0,0 +1,15 @@
+package adrianhitz.adventofcode
+
+import adrianhitz.adventofcode.Day05.Day05Computer
+
+//noinspection ZeroIndexToHead
+object Day08 extends AdventIO {
+  override def main(args: Array[String]): Unit = {
+    write1(part1.toString)
+    write2(part2.toString)
+  }
+
+  def part1(implicit s: String): Int = ???
+
+  def part2(implicit s: String): Int = ???
+}
