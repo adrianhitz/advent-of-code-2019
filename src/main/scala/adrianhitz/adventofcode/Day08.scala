85da592f1fdbdacd6b7a166b9f029baa85fe12d9
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-08 08:47:38 +0100

    Day 08 part 1 solution

--- src/main/scala/adrianhitz/adventofcode/Day08.scala
+++ src/main/scala/adrianhitz/adventofcode/Day08.scala
@@ -2,14 +2,17 @@ package adrianhitz.adventofcode
 
 import adrianhitz.adventofcode.Day05.Day05Computer
 
-//noinspection ZeroIndexToHead
 object Day08 extends AdventIO {
   override def main(args: Array[String]): Unit = {
     write1(part1.toString)
     write2(part2.toString)
   }
 
-  def part1(implicit s: String): Int = ???
+  def part1(implicit s: String): Int = {
+    val pixels: Iterator[List[Int]] = s.toList.map(_.asDigit).grouped(150)
+    val layerWithFewestZeroes: Seq[Int] = pixels.map(x => (x, x.count(_ == 0))).minBy(_._2)._1
+    layerWithFewestZeroes.count(_ == 1) * layerWithFewestZeroes.count(_ == 2)
+  }
 
   def part2(implicit s: String): Int = ???
 }
