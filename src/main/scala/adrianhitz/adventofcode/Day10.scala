cd3958e74cc5d8badce4ae01038a3bcb3ae19bc3
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-10 06:07:59 +0100

    Add day 10 files

---
+++ src/main/scala/adrianhitz/adventofcode/Day10.scala
@@ -0,0 +1,12 @@
+package adrianhitz.adventofcode
+
+object Day10 extends AdventIO {
+  override def main(args: Array[String]): Unit = {
+    write1(part1.toString)
+    write2(part2.toString)
+  }
+
+  def part1(implicit s: String): Int = ???
+
+  def part2(implicit s: String): Int = ???
+}
