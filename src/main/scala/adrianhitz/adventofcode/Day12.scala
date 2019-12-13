10f8c57f73623a9d4c63a204dc0db3f4105fcc00
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-13 07:58:47 +0100

    Day 12 part 1 solution

--- src/main/scala/adrianhitz/adventofcode/Day12.scala
+++ src/main/scala/adrianhitz/adventofcode/Day12.scala
@@ -8,38 +8,41 @@ object Day12 extends AdventIO {
 
   def part1(implicit s: String): Int = {
     val moons: Vector[Moon] = parseInput(s)
-    for(_ <- 0 until 1000) {
+    for (i <- 1 to 1000) {
       // Apply gravity
-      for(moon <- moons) for(other <- moons.filter(_ != moon)) {
-        for(d <- 0 until 3) {
-          if(moon.pos(d) > other.pos(d)) {
+      for (moon <- moons) for (other <- moons.filter(_ != moon)) {
+        for (d <- 0 until 3) {
+          if (moon.pos(d) > other.pos(d)) {
             other.v(d) += 1
-          } else if(moon.pos(d) < other.pos(d)) {
+          } else if (moon.pos(d) < other.pos(d)) {
             other.v(d) -= 1
           }
         }
       }
       // Move
-      for(moon <- moons) {
-        for(d <- 0 until 3) {
+      for (moon <- moons) {
+        for (d <- 0 until 3) {
           moon.pos(d) += moon.v(d)
         }
       }
     }
-
-    moons.map(m => m.pos.map(Math.abs).sum + m.v.map(Math.abs).sum).sum
+    moons.map(m => m.pos.map(Math.abs).sum * m.v.map(Math.abs).sum).sum
   }
 
   def part2(implicit s: String): Int = ???
 
   private class Moon(var pos: Array[Int]) {
     var v: Array[Int] = Array[Int](0, 0, 0)
+
+    override def toString: String = {
+      s"Moon(pos=(${pos(0)}, ${pos(1)}, ${pos(2)}) v=(${v(0)}, ${v(1)}, ${v(2)}))"
+    }
   }
 
   private def parseInput(s: String): Vector[Moon] = {
     s.split('\n')
-        .map(line => line.substring(1, line.length - 1).split(", ").map(x => x.substring(2).toInt))
-        .map(new Moon(_))
-        .toVector
+      .map(line => line.substring(1, line.length - 1).split(", ").map(x => x.substring(2).toInt))
+      .map(new Moon(_))
+      .toVector
   }
 }
