769e6398e5894ea6926c2686a0dd852f81e8eba6
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-13 06:22:29 +0100

    Day 12 partial solution

--- src/main/scala/adrianhitz/adventofcode/Day12.scala
+++ src/main/scala/adrianhitz/adventofcode/Day12.scala
@@ -6,7 +6,22 @@ object Day12 extends AdventIO {
     write2(part2.toString)
   }
 
-  def part1(implicit s: String): Int = ???
+  def part1(implicit s: String): Int = {
+    val moons = parseInput(s)
+    0
+  }
 
   def part2(implicit s: String): Int = ???
+
+  private class Moon(var position: (Int, Int, Int)) {
+    var velocity: (Int, Int, Int) = (0, 0, 0)
+  }
+
+  private def parseInput(s: String): Vector[Moon] = {
+    s.split('\n')
+        .map(line => line.substring(1, line.length - 1).split(", ").map(x => x.substring(2).toInt))
+        .map(x => (x(0), x(1), x(2)))
+        .map(new Moon(_))
+        .toVector
+  }
 }
