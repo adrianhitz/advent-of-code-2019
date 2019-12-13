0663fdf7c674033a8c22837811489ec59efe7746
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2019-12-13 05:54:39 +0100

    Day 10 part 2 solution

--- src/main/scala/adrianhitz/adventofcode/Day10.scala
+++ src/main/scala/adrianhitz/adventofcode/Day10.scala
@@ -1,5 +1,7 @@
 package adrianhitz.adventofcode
 
+import scala.annotation.tailrec
+
 object Day10 extends AdventIO {
   override def main(args: Array[String]): Unit = {
     write1(part1.toString)
@@ -10,8 +12,8 @@ object Day10 extends AdventIO {
   def part1(implicit s: String): Int = {
     val asteroidPositions: Set[(Int, Int)] = parseInput(s)
     val station = asteroidPositions
-      .map(asteroid => (asteroid, (asteroidPositions - asteroid).map(x => reduce(subtract(x, asteroid))).size))
-      .maxBy(_._2)
+        .map(asteroid => (asteroid, (asteroidPositions - asteroid).map(x => reduce(subtract(x, asteroid))).size))
+        .maxBy(_._2)
     stationPosition = station._1
     station._2
   }
@@ -19,28 +21,28 @@ object Day10 extends AdventIO {
   def part2(implicit s: String): Int = {
     case class Asteroid(position: (Int, Int), relativePosition: (Int, Int), distance: Double, reduced: (Int, Int), angle: Double)
     val asteroidPositions: Set[(Int, Int)] = parseInput(s) - stationPosition
-    var asteroids = asteroidPositions.map(x => {
-      val relative = subtract(x, stationPosition)
+    var asteroids = asteroidPositions.map(position => {
+      val relative = subtract(position, stationPosition)
       val distance = Math.pow(relative._1, 2) + Math.pow(relative._2, 2)
-      Asteroid(x, relative, distance, reduce(relative), angle(x))
+      Asteroid(position, relative, distance, reduce(relative), angle(relative))
     })
-    var shot = Vector[Asteroid]()
-    while (asteroids.nonEmpty) {
-      var shooting = Map[(Int, Int), Asteroid]()
 
-      for (asteroid <- asteroids) {
-        if (!shooting.contains(asteroid.reduced)) {
+    var shot = Vector[Asteroid]()
+    while(asteroids.nonEmpty) {
+      var shooting = Map[(Int, Int), Asteroid]()
+      for(asteroid <- asteroids) {
+        if(!shooting.contains(asteroid.reduced)) {
           shooting += asteroid.reduced -> asteroid
-        } else if (asteroid.distance < shooting(asteroid.reduced).distance) {
+        } else if(asteroid.distance < shooting(asteroid.reduced).distance) {
           shooting += asteroid.reduced -> asteroid
         }
       }
-      shot ++= shooting.values.toVector.sortBy(a => a.angle)
+      shot = shot.appendedAll(shooting.values.toVector.sortBy(a => a.angle))
       asteroids --= shooting.values
     }
 
-    val `200` = shot(200).position
-    `200`._1 * 100 + `200`._2
+    val a200 = shot(199).position
+    a200._1 * 100 + a200._2
   }
 
   var stationPosition: (Int, Int) = (0, 0)
@@ -50,23 +52,9 @@ object Day10 extends AdventIO {
       row => row._1.zipWithIndex.filter(elem => elem._1 == '#').map(elem => (elem._2, row._2))
     ).toSet
 
-  @deprecated
-  private def parseInputOld(s: String): Set[(Int, Int)] = {
-    val rows = s.split('\n')
-    var asteroids = Set[(Int, Int)]()
-    for ((row, y) <- rows.zipWithIndex) {
-      for ((elem, x) <- row.toCharArray.zipWithIndex) {
-        if (elem == '#') {
-          asteroids += ((x, y))
-        }
-      }
-    }
-    asteroids
-  }
-
-  @scala.annotation.tailrec
+  @tailrec
   private def gcd(a: Int, b: Int): Int = {
-    if (b == 0) return a
+    if(b == 0) return a
     gcd(b, a % b)
   }
 
@@ -83,12 +71,12 @@ object Day10 extends AdventIO {
     (fraction1._1 - fraction2._1, fraction1._2 - fraction2._2)
 
   private def angle(m: (Int, Int)): Double = {
-    val x = m._1
-    val y = m._2
-    var angle = if (x == 0 && y > 0) Math.PI / 2 else if (x == 0) Math.PI / (-2) else Math.atan(y.toDouble / x.toDouble)
-    if (x < 0) angle += Math.PI
-    angle -= Math.PI / 2
-    if (angle < 0) angle += 2 * Math.PI
+    var angle = m match {
+      case (0, y) if y > 0 => Math.PI / (-2)
+      case (0, _) => Math.PI / 2
+      case (x, y) => Math.atan(y.toDouble / x.toDouble)
+    }
+    if(m._1 < 0) angle += Math.PI
     angle
   }
 }
